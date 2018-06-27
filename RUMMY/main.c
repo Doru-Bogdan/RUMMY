8411fdd16587350c666f81a21e3cfc5da080b274
Doru Bogdan <dorumancila@dorus-macbook-pro.local>
2018-06-27 23:39:12 +0300

    Initial Commit

---
+++ RUMMY/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  RUMMY
+//
+//  Created by Doru Bogdan Mancila on 27/06/2018.
+//  Copyright © 2018 Doru Bogdan Mancila. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
